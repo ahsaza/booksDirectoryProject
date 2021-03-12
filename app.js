0221347682a1be4391225acb2efc773e3627abc4
ahsaza <ahsaza@protonmail.com>
2021-03-12 21:09:57 +0000

    Adding PUT & DELETE functions to the project and using functions instead of repeat

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
 const express = require("express");
 const app = express();
-
 const fs = require("fs");
-
 const PORT = process.env.PORT || 3000;
-let booksArray = require('./books');
+
+const readBooksJSON = (filePath) => {
+    try {
+        return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    catch (err) { throw err };
+}
+
+const WriteToBooksJSON = (filePath, data) => {
+    fs.writeFile(filePath, JSON.stringify(data, null, 4), (err) => {
+        if (err) return console.log(err);
+    });
+}
 
 app.get("/books", (req, res) => {
-    fs.readFile('books.json', 'utf-8', (err, data) => {
-        if (err) {
-            throw err;
-        }
-        booksArray = JSON.parse(data.toString());
-    });
-    res.status(200).json(booksArray.books);
+    const booksJson = readBooksJSON("./books.json");
+    res.status(200).json(booksJson["books"]);
 });
 
 app.get("/books/:id", (req, res) => {
     let bookId = req.params.id;
-    let foundBook = booksArray.books.filter(book => book.id == bookId);
+    const booksJson = readBooksJSON("./books.json");
+    let foundBook = booksJson.books.length > 0 ? booksJson.books.filter(book => book.id == bookId) : [];
     res.status(200).json(foundBook.length === 0 ? "Book Not Found" : foundBook[0]);
 })
 
 app.post("/books/:name", (req, res) => {
-    console.log(booksArray);
+    const booksJson = readBooksJSON("./books.json");
     let addedBook = {
-        id: booksArray.books.length + 1,
+        id: booksJson.books.length > 0 ? booksJson.books[booksJson.books.length - 1].id + 1 : 1,
         bookName: req.params.name
     }
-    booksArray.books.push(addedBook);
-    fs.writeFile('./books.json', JSON.stringify(booksArray, null, 4), (err) => {
-        if (err) return console.log(err);
-    });
-    console.log(booksArray);
+    booksJson.books.push(addedBook);
+    WriteToBooksJSON("./books.json", booksJson);
     res.status(200).send(addedBook);
+});
+
+app.delete("/books/:id", (req, res) => {
+    const booksJson = readBooksJSON("./books.json");
+    booksJson.books = booksJson.books.filter(book => req.params.id != book.id);
+    WriteToBooksJSON("./books.json", booksJson);
+    res.status(200).send("Book Deleted Succesfuly");
+})
+
+app.put("/books", (req, res) => {
+    let { bookId, bookNewName } = req.query;
+    const booksJson = readBooksJSON("./books.json");
+    if (!bookId || !bookNewName) return res.status(400).send("bookId & bookNewName is Mandatory"); 
+    booksJson.books.forEach(book => {
+        book.id == bookId ? book.bookName = bookNewName : "";
+    });
+    WriteToBooksJSON("./books.json", booksJson);
+    res.status(200).send("Book Edited Succisfuly");
 })
 
 app.listen(PORT);
